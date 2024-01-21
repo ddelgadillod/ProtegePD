9464b56861fc0ef0457df4474ee67eace5bb6394
ddelgadillod <diegodelgadilloduran@gmail.com>
2024-01-20 22:54:50 -0500

    uploading installation scripts v3

--- external_dep.sh
+++ external_dep.sh
@@ -1,9 +1,12 @@
 #!/bin/bashi
 echo 'Download muscle'
+cd ProtegePD
 wget https://drive5.com/muscle/downloads3.8.31/muscle3.8.31_i86linux32.tar.gz
 tar -xzvf muscle3.8.31_i86linux32.tar.gz
 rm muscle3.8.31_i86linux32.tar.gz
 chmod +x muscle3.8.31_i86linux32
+mkdir -p bin
 mv muscle3.8.31_i86linux32 ./bin/muscle
-./bin/muscle -h
-
+./bin/muscle
+cd ..
+echo $PATH
